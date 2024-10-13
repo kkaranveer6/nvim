4e5b474315a8e696fa378909118911df6ba4deed
Karanveer <karanveerkar@gmail.com>
2024-10-14 08:09:32 +1030

    Added git diff keymap

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -7,7 +7,18 @@ return {
     'sindrets/diffview.nvim',
     config = function()
       require('diffview').setup {}
+
+      local function toggle_diffview()
+        local view = require('diffview.lib').get_current_view()
+
+        if view then
+          vim.cmd 'DiffviewClose'
+        else
+          vim.cmd 'DiffviewOpen'
+        end
+      end
+
+      vim.keymap.set('n', '<leader>gd', toggle_diffview, { desc = 'Toggle Git [D]iff' })
     end,
-    vim.keymap.set('n', '<leader>gd', ':DiffviewOpen<CR>', { desc = 'Git [D]iff' }),
   },
 }
